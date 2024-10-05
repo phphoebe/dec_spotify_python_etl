17b0901558c241495046804f79c050626d2ebff5
Phoebe Chen <phoebehch@gmail.com>
2024-10-05 13:02:00 -0400

    Add SQL Transform

---
+++ app/etl_project/sql/genre_track_and_artist_details.sql
@@ -0,0 +1,38 @@
+with track_artist as (
+
+	select
+		track_id,
+		track_name,
+		unnest(string_to_array(artist_id, ', ')) as artist_id
+	from 
+        tracks
+
+), artist as (
+
+	select
+		artist_id,
+		artist_name,
+		artist_genres,
+		unnest(string_to_array(regexp_replace(artist_genres, '[{}"]', '', 'g'), ',')) as genre
+	from 
+        artists
+
+)
+select
+	a.genre,
+	count(distinct t.track_id) as num_tracks,
+	count(distinct a.artist_id) as num_artists,
+	string_agg(distinct t.track_name, ', ') as track_names,
+	string_agg(distinct a.artist_name, ', ') as artist_names
+
+from 
+    track_artist as t
+left join artist as a
+	on t.artist_id = a.artist_id
+
+where a.genre is not null
+
+group by 1
+
+order by 
+    2 desc, 3 desc
---
+++ app/etl_project/sql/most_popular_tracks_with_duration.sql
@@ -0,0 +1,16 @@
+select
+	track_id,
+	track_name,
+	track_popularity,
+    concat(
+        (track_duration_ms / 60000), 
+        ':', 
+        lpad(cast((track_duration_ms % 60000) / 1000 as varchar), 2, '0')
+    )  as track_duration_min, 
+	dense_rank() over (order by track_popularity desc) as popularity_rnk
+
+from 
+    tracks
+
+order by 
+    popularity_rnk
