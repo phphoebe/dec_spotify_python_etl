0515d4d145499644009af5f3079ad612c164e4fc
Phoebe Chen <phoebehch@gmail.com>
2024-10-05 10:00:19 -0400

    Update connectors tests

--- app/etl_project_tests/connectors/test_spotify.py
+++ app/etl_project_tests/connectors/test_spotify.py
@@ -1,7 +1,7 @@
-from dotenv import load_dotenv
-from etl_project.connectors.spotify import SpotifyAccessTokenClient, SpotifyAPIClient
 import os
+from dotenv import load_dotenv
 import pytest
+from etl_project.connectors.spotify import SpotifyAccessTokenClient, SpotifyAPIClient
 
 
 @pytest.fixture
@@ -29,16 +29,22 @@ def test_spotify_api_client_get_playlist(setup):
         client_id=client_id, client_secret=client_secret
     )
     spotify_api_client = SpotifyAPIClient(
-        access_token_client=spotify_access_token_client)
+        access_token_client=spotify_access_token_client
+    )
 
     playlist_id = "31FWVQBp3WQydWLNhO0ACi"  # Lofi Girl's favorite playlist
-    playlist_data = spotify_api_client.get_playlist_data(playlist_id)
 
+    # Fetch playlist data with pagination considered
+    playlist_data, all_tracks = spotify_api_client.get_playlist_data(
+        playlist_id)
+
+    # Ensure that the first element is a dictionary (playlist metadata)
     assert type(playlist_data) == dict
     assert "tracks" in playlist_data
 
-    # Check if playlist contains any tracks
-    assert len(playlist_data["tracks"]["items"]) > 0
+    # Ensure that the second element is a list (all tracks)
+    assert type(all_tracks) == list
+    assert len(all_tracks) > 0
 
 
 def test_spotify_api_client_get_artist_from_playlist(setup):
@@ -48,13 +54,18 @@ def test_spotify_api_client_get_artist_from_playlist(setup):
         client_id=client_id, client_secret=client_secret
     )
     spotify_api_client = SpotifyAPIClient(
-        access_token_client=spotify_access_token_client)
+        access_token_client=spotify_access_token_client
+    )
 
     playlist_id = "31FWVQBp3WQydWLNhO0ACi"  # Lofi Girl's favorite playlist
-    playlist_data = spotify_api_client.get_playlist_data(playlist_id)
+
+    # Fetch playlist data considering pagination
+    playlist_data, all_tracks = spotify_api_client.get_playlist_data(
+        playlist_id)
 
     # Extract artist ID from the first track in the playlist
-    first_track = playlist_data["tracks"]["items"][0]
+    # Access tracks from `all_tracks` (second element of the tuple)
+    first_track = all_tracks[0]
     first_artist_id = first_track["track"]["artists"][0]["id"]
 
     # Fetch artist data using the extracted artist ID
