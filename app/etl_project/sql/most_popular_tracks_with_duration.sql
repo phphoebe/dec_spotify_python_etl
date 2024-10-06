634cbf5c13a358d64a3ea772772bcdedc512bfcc
Pheebs <phoebehch@gmail.com>
2024-10-05 20:00:22 -0400

    Update most_popular_tracks_with_duration.sql

--- app/etl_project/sql/most_popular_tracks_with_duration.sql
+++ app/etl_project/sql/most_popular_tracks_with_duration.sql
@@ -1,13 +1,13 @@
 select
-	track_id,
-	track_name,
-	track_popularity,
+    track_id,
+    track_name,
+    track_popularity,
     concat(
-        (track_duration_ms / 60000), 
-        ':', 
-        lpad(cast((track_duration_ms % 60000) / 1000 as varchar), 2, '0')
+	(track_duration_ms / 60000), 
+	':', 
+	lpad(cast((track_duration_ms % 60000) / 1000 as varchar), 2, '0')
     ) as track_duration_min, 
-	dense_rank() over (order by track_popularity desc) as popularity_rnk
+    dense_rank() over (order by track_popularity desc) as popularity_rnk
 
 from 
     tracks
