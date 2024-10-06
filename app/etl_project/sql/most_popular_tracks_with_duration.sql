99c1c7e62e63b4fbc62007d5108deb58379fa92c
Phoebe Chen <phoebehch@gmail.com>
2024-10-05 20:01:08 -0400

    Update SQL Formatting

--- app/etl_project/sql/most_popular_tracks_with_duration.sql
+++ app/etl_project/sql/most_popular_tracks_with_duration.sql
@@ -3,9 +3,9 @@ select
     track_name,
     track_popularity,
     concat(
-	(track_duration_ms / 60000), 
-	':', 
-	lpad(cast((track_duration_ms % 60000) / 1000 as varchar), 2, '0')
+	    (track_duration_ms / 60000), 
+	    ':', 
+	    lpad(cast((track_duration_ms % 60000) / 1000 as varchar), 2, '0')
     ) as track_duration_min, 
     dense_rank() over (order by track_popularity desc) as popularity_rnk
 
